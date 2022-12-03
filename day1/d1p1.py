d540fd27dea9ebe7b43f33f942088e127ba11796
Paris <9128zofia@proton.me>
2022-12-03 13:30:11 +0000

    Day 1

---
+++ day1/d1p1.py
@@ -0,0 +1,15 @@
+with open('day1\d1.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+cal = 0
+elf = 0
+calperelf = []
+
+for x in range(0, len(lines)):
+    if lines[x] == '':
+        calperelf.append(cal)
+        cal = 0
+    else:
+        cal += int(lines[x])
+
+print(sorted(calperelf, reverse=True)[:1])
