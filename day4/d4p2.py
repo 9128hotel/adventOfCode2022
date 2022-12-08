6067263514ecda387ddeede9c5b8d416ee9fcd16
Paris <9128zofia@proton.me>
2022-12-08 12:03:21 +0000

    Day 4 - Updated notes and added Part 1

--- day4/d4p2.py
+++ day4/d4p2.py
@@ -1,6 +1,13 @@
 with open('d4.txt') as f:
     lines = [line.rstrip() for line in f]
 
+# Black magic to check if two lists overlap
+# This method uses sets to create the ranges from the given lower and upper bounds, and then uses the & (intersection) operator to find the overlap, if any, between the two sets. The bool function is then used to convert the resulting set to a boolean value, which will be True if the sets have any elements in common, and False otherwise.
+def is_overlapping(range1, range2):
+    lower1, upper1 = range1
+    lower2, upper2 = range2
+    return bool(set(range(lower1, upper1+1)) & set(range(lower2, upper2+1)))
+
 n1p1 = []
 n1p2 = []
 n2p1 = []
@@ -38,11 +45,9 @@ while index < len(lines):
     n2p2.append(int(lines[index]))
     index +=4
 
-# Check if one range contains the other
+# Check if one range overlaps the other
 for x in range(len(n1p1)):
-    if (n1p1[x] >= n2p1[x] and n2p2[x] >= n1p2[x]):
-        score += 1
-    elif (n1p1[x] <= n2p1[x] and n2p2[x] <= n1p2[x]):
+    if is_overlapping((n1p1[x], n1p2[x]), (n2p1[x], n2p2[x])):
         score += 1
 
 input(score)
